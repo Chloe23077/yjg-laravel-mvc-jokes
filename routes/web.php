fb96718ec11aa7399ec9f2bf10123b0a9bd90415
Yunjeong Gu <yunjasgu@gmail.com>
2024-11-13 10:01:40 +0800

    feat(static-pages): Add authenticated user content

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', [\App\Http\Controllers\StaticController::class, 'home'])->name('static.home');
+Route::get('/', [\App\Http\Controllers\StaticController::class, 'home'])->name('static.home');
 Route::get('/', [\App\Http\Controllers\StaticController::class, 'home'])->name('welcome');
 Route::get('/about', [\App\Http\Controllers\StaticController::class, 'about'])->name('about');
 Route::get('/contact', [\App\Http\Controllers\StaticController::class, 'contact'])->name('contact');
